b14da63f7789cf302c9dc8b7da9dcb20f6087569
van <vgalvoso@gmail.com>
2022-09-22 03:41:27 +0800

    update title

--- view/section/header.php
+++ view/section/header.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="public/res/img/logoblue.png" type="image/x-icon">
-    <title>PHTMX</title>
+    <title>PHunctional PHP</title>
     <link rel="stylesheet" href="public/css/mystyle.css"> 
     <script src="public/js/htmx.min.js"></script>
     <script src="public/js/vanscript.js"></script>
